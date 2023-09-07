511301ae2aedab5a604f172ce2d1dc876eb4e6e3
Olivier Vastel <vastel.olivier@gmail.com>
2023-09-07 10:31:34 +0200

    Exo 3

---
+++ Échauffement Articulaire/Exercice 3/app.js
@@ -0,0 +1,10 @@
+/*  Écrivez un programme qui affiche les nombres de 1 à 10 en utilisant une boucle `for`. */
+
+function enumeration(){
+    for (var i = 1 ; i <= 10 ; i++ ){
+        console.log(i)
+    }
+
+}
+
+enumeration()
